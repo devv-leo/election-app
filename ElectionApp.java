addaf73db62ca34a2c0597c9a3abd7103a001645
devv-leo <lekan.code@gmail.com>
2026-03-29 18:41:09 +0100

    Implement basic storage system with ArrayLists and add/remove methods

--- ElectionApp.java
+++ ElectionApp.java
@@ -1,9 +1,36 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ElectionApp {
+    private ArrayList<Candidate> candidates;
+    private ArrayList<Voter> voters;
+    private Scanner scanner;
+
+    public ElectionApp() {
+        this.candidates = new ArrayList<>();
+        this.voters = new ArrayList<>();
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void addCandidate(Candidate candidate) {
+        candidates.add(candidate);
+    }
+
+    public void addVoter(Voter voter) {
+        voters.add(voter);
+    }
+
+    public ArrayList<Candidate> getCandidates() {
+        return candidates;
+    }
+
+    public ArrayList<Voter> getVoters() {
+        return voters;
+    }
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Election App Started");
-        scanner.close();
+        ElectionApp app = new ElectionApp();
+        System.out.println("Welcome to my Election App!");
+        app.scanner.close();
     }
 }
